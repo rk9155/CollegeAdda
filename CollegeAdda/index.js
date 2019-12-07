eb1395a5b6a3d255494aa27f7714f38b5e0a2710
RAKESH KUMAR <47426714+rk9155@users.noreply.github.com>
2019-12-07 23:46:04 +0530

    Update index.js
    Google sign in added

--- CollegeAdda/index.js
+++ CollegeAdda/index.js
@@ -1,23 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-
-
-const app = express();
-
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+var passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+const findOrCreate = require('mongoose-findorcreate');
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const app = express();
-const userSchema = {
-    email: String,
-    Password: String
-};
-
-const User = new mongoose.model("User", userSchema);
-
-
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -25,45 +18,118 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+// app.use(session({
+//     secret: "Our little secret.",
+//     resave: false,
+//     saveUninitialized: false
+// }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String
+});
+//UserSchema
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+const User = new mongoose.model("User", userSchema);
+
+//passport
+
+passport.use(User.createStrategy());
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+//google signIn
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/collegeadda",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+app.get("/auth/google",
+    passport.authenticate('google', { scope: ["profile"] })
+);
+app.get("/auth/google/collegeadda",
+    passport.authenticate('google', { failureRedirect: "/login" }),
+    function (req, res) {
+        // Successful authentication, redirect to home.
+        res.redirect('/');
+    });
+
+//login
 app.get("/login", function (req, res) {
     res.render("login");
 });
-
+//register
 app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        Password: req.body.password
-    });
+    // User.findById(req.user.id, function (err, foundUser) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (foundUser) {
+    //             foundUser.secret = submittedSecret;
+    //             foundUser.save(function () {
+    //                 res.redirect("/");
+    //             });
+    //         }
+    //     }
+    // });
 
-    newUser.save(function (err) {
+app.post("/register", function (req, res) {
+    User.register({ 
+        username: req.body.username 
+        }, 
+        req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect('/');
+            });
+        }
+    });
+});
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function (err) {
         if (err) {
             console.log(err);
         } else {
-            res.render('/');
+            passport.authenticate("local")(req, res, function () {
+                res.redirect('/');
+            });
         }
     });
 });
 
-app.post("/login", function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({email: username}, function(err, foundUser){
-        if(err){
-            console.log(err);
-        }else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render('Home');
-                }
-            }
-        }
-    });
-});
 
 const getProducts = require('./Routes/getProducts');
 const upload = require('./Routes/upload');
