e539ec4f826448d484b7623e633e4dda2f46307b
RAKESH KUMAR <47426714+rk9155@users.noreply.github.com>
2019-12-07 21:01:13 +0530

    Update index.js
    Login added

--- CollegeAdda/index.js
+++ CollegeAdda/index.js
@@ -1,5 +1,70 @@
 const express = require('express');
+
+
 const app = express();
+
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const app = express();
+const userSchema = {
+    email: String,
+    Password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        Password: req.body.password
+    });
+
+    newUser.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('/');
+        }
+    });
+});
+
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render('Home');
+                }
+            }
+        }
+    });
+});
+
 const getProducts = require('./Routes/getProducts');
 const upload = require('./Routes/upload');
 const home = require('./Routes/Home');
