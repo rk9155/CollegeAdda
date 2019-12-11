d37d92e1f25748734d8d03ad0cf3ea3172f507e4
Yash Janoria <44284086+Yash2412@users.noreply.github.com>
2019-12-11 10:15:19 +0530

    Update index.js

--- CollegeAdda/index.js
+++ CollegeAdda/index.js
@@ -140,11 +140,13 @@ const getProducts = require('./Routes/getProducts');
 const upload = require('./Routes/upload');
 const home = require('./Routes/Home');
 const post = require('./Routes/post');
+const items = require('./Routes/items');
 
 app.use(express.static('./Template'));
 app.use('/', home);
 app.use('/post', post);
 app.use('/getProducts', getProducts);
 app.use('/upload', upload);
+app.use('/items' ,items);
 
 app.listen("3000");
