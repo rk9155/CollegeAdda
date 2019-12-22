cb9ff03f5d90b65a07695aa6287ff21fa9e13972
Yash Janoria <yashjanoria111@gmail.com>
2019-12-22 06:44:20 +0530

    Fifth commit

--- Routes/Home.js
+++ Routes/Home.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const products = require('../Models/product');
+
 
 
 router.get('/', (req, res) => {
-    res.render('home2',{
-        user: req.user ? req.user : ""
-    });
+    async function getProducts(){
+        const product = await products.find({})
+        .limit(12)
+        .sort({date: 1})
+
+        res.render('home2',{
+            user: req.user ? req.user : "",
+            product: product
+        });
+    }
+
+    getProducts();
 });
 
 module.exports = router;
---
+++ Routes/image.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const Grid = require('gridfs-stream');
+const mongoose = require('mongoose');
+
+const conn = mongoose.createConnection('mongodb+srv://Yash2412:yash2412@cluster0-6rqau.mongodb.net/college-adda');
+let gfs;
+conn.once('open', function () {
+    gfs = Grid(conn.db, mongoose.mongo);
+  })
+
+router.get('/:filename' ,(req,res) =>{
+        async function getImg(){
+        const image = await gfs.files.findOne({filename: req.params.filename})
+        if(image){
+            var readstream = gfs.createReadStream(image.filename);
+            readstream.pipe(res);
+        }
+        else{
+            res.send({err: 'no image available'});
+        }        
+    }
+
+    getImg();
+});
+
+
+module.exports = router;
