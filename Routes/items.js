f68af42af06bd88a2e43be6148bd03489e7ac881
Yash Janoria <yashjanoria111@gmail.com>
2019-12-22 07:41:00 +0530

    sixth commit

--- Routes/items.js
+++ Routes/items.js
@@ -10,7 +10,10 @@ router.get('/:title' , async (req,res) =>{
     const title = req.params.title;
     async function getProducts(){
         const product = await products.find({title: title}).limit(1);
-        res.render('sell_item', product[0]);
+        res.render('sell_item', {
+            product: product[0],
+            user: req.user
+        });
     }
 
     getProducts();
